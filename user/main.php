00299772e0c5d0642b53a2dfe85f111c955001a1
Couprie <jarod64@hotmail.fr>
2023-07-04 00:31:04 +0200

    active page in icon colored

--- user/main.php
+++ user/main.php
@@ -26,10 +26,52 @@ require_once("../tools/protect.php");
       echo "<p>Bienvenue " . $_SESSION["userFirstName"] . " " . $userLastName = $_SESSION["userName"] . " !";
       ?>
     </section>
-    <section id="section-2">Canal général</section>
-    <section id="section-3">Canal groupe</section>
-    <section id="section-4">Meteo</section>
-    <section id="section-5">Mails</section>
+    <section id="section-2">
+      <?php
+      require_once("../tools/connect.php");
+      $sql = "SELECT * FROM message
+      INNER JOIN user ON message.user_id = user.user_id
+      INNER JOIN appartient ON message.msg_id = appartient.msg_id
+      WHERE appartient.canal_id = 1
+      ORDER BY msg_date_creation DESC";
+      $recordset = $db->query($sql);
+      foreach ($recordset as $row) { ?>
+        <div class="gen-message">
+          <p>
+            <?= htmlspecialchars($row["msg_content"]); ?>
+          </p>
+          <p>
+            <?= htmlspecialchars($row["user_name"]) . " " . htmlspecialchars($row["user_name"]) . " · " . htmlspecialchars($row["msg_date_creation"]); ?>
+          </p>
+        </div>
+      <?php } ?>
+    </section>
+    <section id="section-3">
+      <?php
+      require_once("../tools/connect.php");
+      $sql = "SELECT * FROM message
+      INNER JOIN appartient ON message.msg_id = appartient.msg_id
+      INNER JOIN canal ON appartient.canal_id = canal.canal_id
+      INNER JOIN voir ON canal.canal_id = voir.canal_id
+      INNER JOIN role ON voir.role_id = role.role_id
+      INNER JOIN possede ON role.role_id = possede.role_id
+      INNER JOIN user ON possede.user_id = user.user_id
+      WHERE message.user_id = user.user_id
+      AND possede.main_role = 1";
+      $recordset = $db->query($sql);
+      foreach ($recordset as $row) { ?>
+        <div class="gen-message">
+          <p>
+            <?= htmlspecialchars($row["msg_content"]); ?>
+          </p>
+          <p>
+            <?= htmlspecialchars($row["user_name"]) . " " . htmlspecialchars($row["user_name"]) . " · " . htmlspecialchars($row["msg_date_creation"]); ?>
+          </p>
+        </div>
+      <?php } ?>
+    </section>
+    <section id="section-4">Incoming feature (Meteo)</section>
+    <section id="section-5">Incoming feature (Mails)</section>
   </main>
 </body>
 
